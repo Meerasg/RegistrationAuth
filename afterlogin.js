ca04b04c0e2c14fbd70ce0a78d168e5b8d93a46b
Ryan Dsilva <ryan.dsilva.98@gmail.com>
2019-10-21 00:01:17 +0530

    changes and bug fixes

--- afterlogin.js
+++ afterlogin.js
@@ -1,31 +1,15 @@
-function login()
-{
-    var email=document.getElementById("email_field").value;
-    var pass=document.getElementById("password_field").value;
+function login() {
+  var email = document.getElementById("email_field").value;
+  var pass = document.getElementById("password_field").value;
 
-   
-firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function(error) {
-    // Handle Errors here.
-
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  }
-  );
-  
-  window.alert('You have successfully signed in');
-  
- 
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, pass)
+    .then(res => {
+      window.alert("You have successfully signed in");
+    })
+    .catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+    });
 }
-
-
-
-
-
-
-
-
-
-
-
-  
